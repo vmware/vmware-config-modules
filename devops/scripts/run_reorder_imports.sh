a7ba7ccbc9de85a23854cfafb2f32e1e6fa20a89
huanggs001 <172428411+huanggs001@users.noreply.github.com>
2024-11-18 11:15:13 -0800

    fix pypi push workflow (#23)
    Co-authored-by: Guangsong Huang <hguangsong@vmware.com>
    Co-authored-by: Cody Douglas <157752937+codydouglasBC@users.noreply.github.com>

--- devops/scripts/run_reorder_imports.sh
+++ devops/scripts/run_reorder_imports.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 echo "============================================="
@@ -6,20 +6,52 @@ echo "Run reorder-python-imports for config-modules"
 echo "============================================="
 python3 -m pip install reorder-python-imports
 
-if [ $# -eq 0 ]
-then
-  echo "No files passed. Running reorder-python-imports on all .py files"
-  FILES_TO_SCAN=$(find . -name "*.py" )
-else
-  FILES_TO_SCAN="$*"
-fi
-while read -r line; do
-  filearray=("$line")
-  for file in $filearray; do
-    if [[ "$file" == *.py && "$file" != *"config_modules_vmware/framework/clients/vcenter/dependencies/"* ]]
-    then
-       reorder-python-imports --exit-zero-even-if-changed "$file"
+run_all_files()
+{
+  FILES_TO_SCAN=$(find . -name "*.py")
+  echo "$FILES_TO_SCAN" | while IFS= read -r file; do
+    if [ "${file##*.}" = "py" ] && [ "${file#*config_modules_vmware/framework/clients/vcenter/dependencies/}" = "$file" ]; then
+      changed_file=$( (reorder-python-imports --exit-zero-even-if-changed "$file" 1>&2) 2>&1)
+        if [ -n "$changed_file" ]
+        then
+          echo "$changed_file"
+        fi
     fi
   done
-done <<< "$FILES_TO_SCAN"
+}
 
+run_passed_files()
+{
+  for file in "$@"
+  do
+    if [ "${file##*.}" = "py" ] && [ "${file#*config_modules_vmware/framework/clients/vcenter/dependencies/}" = "$file" ]; then
+      changed_file=$( (reorder-python-imports --exit-zero-even-if-changed "$file" 1>&2) 2>&1)
+        if [ -n "$changed_file" ]
+        then
+          echo "$changed_file"
+        fi
+    fi
+  done
+}
+
+
+if [ $# -eq 0 ]; then
+  echo "No files passed. Running reorder-python-imports on all .py files"
+  function_output=$(run_all_files)
+  if [ -n "$function_output" ]
+  then
+    echo Files that failed:
+    echo "$function_output"
+    exit 1
+  fi
+
+else
+  echo "Files passed as arguments. Running reorder-python-imports on the passed in files"
+  function_output=$(run_passed_files "$@")
+  if [ -n "$function_output" ]
+  then
+    echo Files that failed:
+    echo "$function_output"
+    exit 1
+  fi
+fi
