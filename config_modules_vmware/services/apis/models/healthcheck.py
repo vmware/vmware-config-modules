1901c13f8107ee367ae78636452f20d676024fa0
sujanabhumi <sujana.bhumireddy@jpmchase.com>
2025-08-13 09:12:58 -0400

    Issue-1: Incorporate feedback from Chris and Mike
    Signed-off-by: sujanabhumi <sujana.bhumireddy@jpmchase.com>

--- config_modules_vmware/services/apis/models/healthcheck.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2024 Broadcom. All Rights Reserved.
-from pydantic import BaseModel
-
-
-class HealthCheck(BaseModel):
-    """Class to validate and return when performing a health check."""
-
-    status: str = "OK"
